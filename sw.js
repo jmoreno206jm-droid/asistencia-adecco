acdab9b40ba89558d470f0ba6b56c60757b46e0b
jmoreno206jm-droid <jmoreno.20.6jm@gmail.com>
2026-01-02 09:34:27 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,16 @@
 const CACHE_NAME = "asistencia-adecco-v1";
 
 const FILES_TO_CACHE = [
-  "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 // Instalar
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
@@ -35,6 +34,14 @@ self.addEventListener("activate", event => {
 // Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, clone);
+        });
+        return res;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
